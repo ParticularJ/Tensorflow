d683cb0b34f0600fa3fee6c36c4dc3ccbb858f9e
ParticularJ <344409038@qq.com>
2017-11-18 16:17:36 +0800

    dnn

---
+++ tensorflow/MLP.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import tensorflow.examples.tutorials.mnist.input_data as input_data
+
+mnist = input_data.read_data_sets("FLAG.data_dir/")
+X_train = mnist.train.images
+X_test = mnist.test.images
+y_train = mnist.train.labels.astype("int")
+y_test = mnist.test.labels.astype("int")
+
+config = tf.contrib.learn.RunConfig(tf_random_seed = 42)
+
+feature_columns = tf.contrib.learn.infer_real_valued_columns_from_input(X_train)
+dnn_clf = tf.contrib.learn.DNNClassifier(hidden_units = [300, 100], n_classes = 10, 
+                                         feature_columns = feature_columns, config = config)
+
+dnn_clf = tf.contrib.learn.SKCompat(dnn_clf)
+
+dnn_clf.fit(x = X_train, y = y_train, batch_size = 50, steps = 10000)
+
+
+from sklearn.metrics import accuracy_score
+
+y_pred = dnn_clf.predict(X_test)
+a = accuracy_score(y_test, y_pred['classes'])
+print("accuracy: ", a)
+
+from sklearn.metrics import log_loss
+
+y_pred_prob = y_pred['probabilities']
+b = log_loss(y_test, y_pred_prob)
+print("log_loss: ",b)
+
+
