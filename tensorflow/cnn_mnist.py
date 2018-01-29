fbfd2fb94f06ac0aea2e6e6475e3654bddec5f75
ParticularJ <344409038@qq.com>
2018-01-29 22:58:05 +0800

    cnn

--- tensorflow/cnn_mnist.py
+++ tensorflow/cnn_mnist.py
@@ -4,4 +4,13 @@ import numpy as np
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
+def cnn_model_fn(features, labels, mode):
+    input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
 
+
+    conv1 = tf.layers.conv2d(
+        inputs = input_layer,                            
+        filters = 32,                       
+        kernel_size = [5, 5],
+        padding = "same",
+        activation = tf.nn.relu)
